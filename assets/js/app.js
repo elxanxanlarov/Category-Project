9977a18c7258268e6a3e9589649f9e8997c3fd38
Elxan Xanlarov <elxanxanlarov@gmail.com>
2024-01-01 11:49:24 +0400

    task completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,47 +1,102 @@
-function fetchProducts() {
-  let cards = document.querySelector(".cards");
-
   fetch("https://fakestoreapi.com/products")
-    .then((res) => res.json())
-    .then((data) => {
+  .then((res) => res.json())
+  .then((data) => {
+    cards.innerHTML=""
       data.forEach((element) => {
-        let { title, price, image } = element;
-
-        cards.innerHTML += `
-          <div class="card-item">
-            <div class="card__img">
-              <img src="${image}" alt="Alt" />
-            </div>
-            <div class="card__content">
-              <p>${title}</p>
-              <div class="card__content-footer">
-                <span class="card__content-footer-price">${price}</span>
-                <span class="card__content-footer-icon">
-                  <i class="fa-solid fa-bag-shopping"></i>
-                </span>
-              </div>
-            </div>
-          </div>
-        `;
+      getData(element)
+      // cardAddClick(element)
+      // console.log(element);
       });
     });
-}
 
-fetchProducts()
 
+
+console.log();
 //! Event listeners
-
-let categoryItems = document.querySelector(".category__items");
+let categoryItems = document.querySelectorAll(".category__item");
 let menuSection = document.querySelector(".menu__section");
 let headerIcon = document.querySelector(".header__icon");
 let menuCancelIcon = document.querySelector(".menu__canel__icon");
+let menuCardBlock=document.querySelector('.menu__card__container')
+let cards = document.querySelector(".cards");
+let menuButton=document.querySelector('.menu__button')
 
+let result=[]
+// ! click
 
+  categoryItems.forEach(item=>{
+    item.addEventListener('click',(e)=>{
+      console.log(item.innerHTML);
+      let url=item.textContent.toLowerCase();
+      fetch(`https://fakestoreapi.com/products/category/${url}`)
+      .then((res)=>res.json())
+      .then(categorys=>{
+        cards.innerHTML=""
+        categorys.forEach(categoryData=>{
+          getData(categoryData)
+        })
+      })
+    })
+    })
+
+    function cardAddClick(e){
+  let cardBlock=e.parentElement.parentElement.parentElement
+  menuButton.classList.remove('dp__none')
+  menuSection.classList.add("active");
+  fetch(`https://fakestoreapi.com/products/${cardBlock.id}`)
+  .then(res=>res.json())
+  .then(product=>{
+    let productPrice=product.price
+    menuCardBlock.innerHTML+=`<div class="menu__cards__block">
+    <div class="menu__img__block">
+      <img src="${product.image}" alt="">
+    </div>
+    <div class="menu__card__info__block">
+      <p class="menu__card__title">${product.title.substring(0, 25).concat('...')}</p>
+      <div class="menu__price__input__block">
+        <p><span class="menu__price">${productPrice}</span>$</p>
+        <input value=1 type="number" onchange='updatePrice(this,${productPrice})'>
+      </div>
+    </div>`
+  })
+    }
+    function updatePrice(e,price){
+    let priceSpan=e.parentElement.firstElementChild.firstElementChild
+    priceSpan.textContent=Number(e.value * price).toFixed(2)
+    console.log(e.value);
+    }
+// ! get data
+function getData(card){
+  let {title,price,image,id}=card
+  cards.innerHTML+=`
+<div class="card-item" id='${id}' >
+<div class="card__img">
+  <img src="${image}" alt="Alt" />
+</div>
+<div class="card__content">
+  <p>${title}</p>
+  <div class="card__content-footer">
+    <span class="card__content-footer-price">${price}</span>
+    <span onclick="cardAddClick(this)" class="card__content-footer-icon">
+      <i class="fa-solid fa-bag-shopping"></i>
+    </span>
+  </div>
+</div>
+</div>
+`
+}
+// function search(serchTherm, key, data){
+//     serchTherm=serchTherm.toLowerCase()
+// let filterData=data.filter(item=>{
+// let currentItemValue=item[key].toLowerCase();
+// return currentItemValue.includes(serchTherm)
+// })
+// return filterData
+// }
 
 headerIcon.addEventListener("click", function () {
   menuSection.classList.add("active");
 });
-
 menuCancelIcon.addEventListener("click", function () {
   menuSection.classList.remove("active");
 });
@@ -49,45 +104,94 @@ menuCancelIcon.addEventListener("click", function () {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // ! filterleme
-
-
-let  category__item=document.querySelectorAll(".category__item")
+// let  category__item=document.querySelectorAll(".category__item")
  
-  category__item.forEach((element)=>{
-    element.addEventListener('click',()=>{
-      let newCategoryText=element.innerHTML
-fetchProducts(newCategoryText);
+//   category__item.forEach((element)=>{
+//     element.addEventListener('click',()=>{
+//       let newCategoryText=element.innerHTML
+// fetchProducts(newCategoryText);
 
 
-    })
-  })
-  function filterCategories(categories){
-    fetch(`https://fakestoreapi.com/products/categories/${categories}`)
-      .then((res)=>res.json())
-      .then((data) => {
-        data.forEach((element) => {
-          console.log(element);
-          let { title, price, image } = element;
+//     })
+//   })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// filterCategories()
+
+
+  // function filterCategories(categories){
+  //   fetch(`https://fakestoreapi.com/products/categories/${categories}`)
+  //     .then((res)=>res.json())
+  //     .then((data) => {
+  //       data.forEach((element) => {
+  //         console.log(element);
+  //         let { title, price, image } = element;
   
-          cards.innerHTML += `
-            <div class="card-item">
-              <div class="card__img">
-                <img src="${image}" alt="Alt" />
-              </div>
-              <div class="card__content">
-                <p>${title}</p>
-                <div class="card__content-footer">
-                  <span class="card__content-footer-price">${price}</span>
-                  <span class="card__content-footer-icon">
-                    <i class="fa-solid fa-bag-shopping"></i>
-                  </span>
-                </div>
-              </div>
-            </div>
-          `;
-        });
-      });
-  }
+  //         cards.innerHTML += `
+  //           <div class="card-item">
+  //             <div class="card__img">
+  //               <img src="${image}" alt="Alt" />
+  //             </div>
+  //             <div class="card__content">
+  //               <p>${title}</p>
+  //               <div class="card__content-footer">
+  //                 <span class="card__content-footer-price">${price}</span>
+  //                 <span class="card__content-footer-icon">
+  //                   <i class="fa-solid fa-bag-shopping"></i>
+  //                 </span>
+  //               </div>
+  //             </div>
+  //           </div>
+  //         `;
+  //       });
+  //     });
+  // }
 
   
